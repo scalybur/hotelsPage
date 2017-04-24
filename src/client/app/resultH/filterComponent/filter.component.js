215b7767b41bf453580c3ef1fee8310c47a7215f
nahuelcabrera <nahuelcabrerawork@gmail.com>
2017-04-24 11:38:40 -0300

    filtersComponents

---
+++ src/client/app/resultH/filterComponent/filter.component.js
@@ -0,0 +1,21 @@
+/**
+ * Created by nahuelcabrera on 24/04/17.
+ */
+(function ()
+{
+    angular.module('hotelsResultModule')
+        .component('filterComponent',
+    {
+        controller:filterController,
+        templateUrl:"./filterComponent.html"
+
+    }).controller('starsController', ['$scope', function($scope) {
+        $scope.checkboxModel = {
+            value1 : true,
+            value2 : 'YES'
+        };
+    }]);
+
+
+
+})();
