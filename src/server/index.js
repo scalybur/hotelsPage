6d05aafbf810622ee178427fdb263829ae10c289
nahuelcabrera <nahuelcabrerawork@gmail.com>
2017-04-27 12:09:06 -0300

    serv

--- src/server/index.js
+++ src/server/index.js
@@ -1,26 +1,22 @@
+//CONSTS
+
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
-
-
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended:true}));
 
-///////////PROJECT/////////////
-
+//Project folder
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/../client/index.html')));
 
-//////////Static Routes/////
+//Static Routes
 
 app.use('/src', express.static(path.join(__dirname, './../../src')));
 app.use('/build', express.static(path.join(__dirname, './../../build')));
 
-
-
-///////LISTENING PORT///////
+//Listening PORT
 
 app.listen(3000);
-console.log('La aplicacion se escucha en el puerto 3000');
-
+console.log('App listening on port 3000');
