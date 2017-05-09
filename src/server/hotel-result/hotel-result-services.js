54377fe2f6301aa3d5a29c264add753e275479f9
nahuelcabrera <nahuelcabrerawork@gmail.com>
2017-05-09 12:28:13 -0300

    i18nLocalePolishPrices

--- src/server/hotel-result/hotel-result-services.js
+++ src/server/hotel-result/hotel-result-services.js
@@ -2,13 +2,13 @@ class HotelResultService
 {
     static readHotels(){
         return [{
-         "_id": {
-           "$oid": "58cb47efe3dcd6c694f67533"
+         "idH": {
+           "$idHH": "AKA34"
          },
          "name": "Kelvin Palace",
          "descrip": "Solo la habitación",
          "stars": "4",
-         "price": 11000.9,
+         "price": 11000,
          "images": [
            "https://images.almundo.com/201/1000000/10000/4200/4164/675037ae_b.jpg",
            "https://images.almundo.com/201/1000000/10000/6300/6260/6b25e36d_b.jpg",
@@ -21,13 +21,13 @@ class HotelResultService
          }
        },
        {
-         "_id": {
-           "$oid": "58cb47efe3dcd6c694f67534"
+         "idH": {
+           "$idHH": "KLK09"
          },
          "name": "Pensilvania Resort & Casino",
          "descrip": "Solo la habitación",
          "stars": "4",
-         "price": 2509.0,
+         "price": 8600,
          "images": [
            "https://images.almundo.com/201/1000000/10000/6300/6260/6b25e36d_b.jpg",
            "https://images.almundo.com/201/1000000/10000/4200/4164/675037ae_b.jpg",
@@ -40,13 +40,13 @@ class HotelResultService
          }
        },
        {
-         "_id": {
-           "$oid": "58cb47efe3dcd6c694f67535"
+         "idH": {
+           "$idHH": "UJH31"
          },
          "name": "House of Love",
          "descrip": "Solo la habitación",
          "stars": "2",
-         "price": 900.0,
+         "price": 4700,
          "images": [
            "https://images.almundo.com/201/1000000/10000/8100/8092/8092_106_b.jpg",
            "https://images.almundo.com/201/1000000/10000/6300/6260/6b25e36d_b.jpg",
@@ -59,13 +59,13 @@ class HotelResultService
          }
        },
        {
-         "_id": {
-           "$oid": "58cb47efe3dcd6c694f67536"
+         "idH": {
+           "$idHH": "TYW32"
          },
          "name": "Disney Magic Island",
          "descrip": "Solo la habitación",
          "stars": "5",
-         "price": 9000.0,
+         "price": 19000,
          "images": [
            "https://images.almundo.com/201/1000000/10000/9300/9279/9279_69_b.jpg",
            "https://placehold.it/350x230",
@@ -78,13 +78,13 @@ class HotelResultService
          }
        },
        {
-         "_id": {
-           "$oid": "58cb47efe3dcd6c694f67537"
+         "idH": {
+           "$idHH": "MNB23"
          },
          "name": "Trebol Resort",
          "descrip": "Solo la habitación",
          "stars": "2",
-         "price": 2000.0,
+         "price": 6100,
          "images": [
            "https://images.almundo.com/201/2000000/1350000/1343800/1343731/1343731_71_b.jpg",
            "https://placehold.it/350x230",
@@ -97,13 +97,13 @@ class HotelResultService
          }
        },
        {
-         "_id": {
-           "$oid": "58cb47efe3dcd6c694f67538"
+         "idH": {
+           "$idHH": "ASD23"
          },
          "name": "Magnity Mountain",
          "descrip": "Solo la habitación",
          "stars": "3",
-         "price": 2540.0,
+         "price": 3699,
          "images": [
            "https://images.almundo.com/201/2000000/1350000/1343800/1343731/1343731_71_b.jpg",
            "https://placehold.it/350x230",
@@ -116,13 +116,13 @@ class HotelResultService
          }
        },
        {
-         "_id": {
-           "$oid": "58cb47efe3dcd6c694f67539"
+         "idH": {
+           "$idHH": "POK31"
          },
          "name": "Empire New Jersey",
          "descrip": "Solo la habitación",
          "stars": "4",
-         "price": 5000.0,
+         "price": 5050,
          "images": [
            "https://images.almundo.com/201/1000000/30000/25000/24954/24954_678_b.jpg",
            "https://placehold.it/350x230",
