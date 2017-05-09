707afc0a705e3218a7288b568042d983cf02903a
nahuelcabrera <nahuelcabrerawork@gmail.com>
2017-05-09 04:44:27 -0300

    polishInProgress

--- src/client/app/resultH/resultHoteles.service.js
+++ src/client/app/resultH/resultHoteles.service.js
@@ -3,7 +3,7 @@
 
     angular
         .module('hotelsResultModule')
-        .factory('hotelsResultService', hotelsService);
+        .factory('hotelsService', hotelsService);
 
     hotelsService.$inject = ['$http'];
 
