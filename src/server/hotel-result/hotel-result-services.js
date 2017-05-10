f143ce79ba91435d3ba7c64c9bdcd5a991a4ff15
nahuelcabrera <nahuelcabrerawork@gmail.com>
2017-05-10 11:12:26 -0300

    buildImagesPolish

--- src/server/hotel-result/hotel-result-services.js
+++ src/server/hotel-result/hotel-result-services.js
@@ -22,6 +22,28 @@ class HotelResultService
          }
        },
        {
+           "idH": {
+               "$idHH": "TYW32"
+           },
+           "name": "Disney Magic Island",
+           "descrip": "Solo la habitación",
+           "stars": "5",
+           "price": 19000,
+           "images": [
+               "https://images.almundo.com/201/1000000/30000/25100/25033/25033_147_b.jpg",
+               "https://images.almundo.com/201/1000000/30000/25100/25033/25033_146_b.jpg",
+               "https://images.almundo.com/201/1000000/30000/25100/25033/25033_166_b.jpg",
+               "https://images.almundo.com/201/1000000/30000/25100/25033/25033_172_b.jpg"
+           ],
+           "amenities": {
+               "wifi": "wifi",
+               "pay": "payform",
+               "par": "Parking",
+               "tel": "telefono",
+               "sp": "Spa"
+           }
+       },
+       {
          "idH": {
            "$idHH": "KLK09"
          },
@@ -60,28 +82,6 @@ class HotelResultService
        },
        {
          "idH": {
-           "$idHH": "TYW32"
-         },
-         "name": "Disney Magic Island",
-         "descrip": "Solo la habitación",
-         "stars": "5",
-         "price": 19000,
-         "images": [
-             "https://images.almundo.com/201/1000000/30000/25100/25033/25033_147_b.jpg",
-             "https://images.almundo.com/201/1000000/30000/25100/25033/25033_146_b.jpg",
-             "https://images.almundo.com/201/1000000/30000/25100/25033/25033_166_b.jpg",
-             "https://images.almundo.com/201/1000000/30000/25100/25033/25033_172_b.jpg"
-         ],
-         "amenities": {
-             "wifi": "wifi",
-             "pay": "payform",
-             "par": "Parking",
-             "tel": "telefono",
-             "sp": "Spa"
-         }
-       },
-       {
-         "idH": {
            "$idHH": "MNB23"
          },
          "name": "Trebol Resort",
@@ -89,9 +89,9 @@ class HotelResultService
          "stars": "2",
          "price": 6100,
          "images": [
-           "https://images.almundo.com/201/2000000/1350000/1343800/1343731/1343731_71_b.jpg",
-           "https://placehold.it/350x230",
-           "https://placehold.it/350x230"
+             "https://images.almundo.com/201/2000000/1350000/1343800/1343731/1343731_71_b.jpg",
+             "https://images.almundo.com/201/1000000/10000/6300/6260/6b25e36d_b.jpg",
+             "https://images.almundo.com/201/1000000/10000/8100/8092/8092_106_b.jpg"
          ],
          "amenities": {
            "tel": "telefono",
@@ -107,9 +107,9 @@ class HotelResultService
          "stars": "3",
          "price": 3699,
          "images": [
-           "https://images.almundo.com/201/2000000/1350000/1343800/1343731/1343731_71_b.jpg",
-           "https://placehold.it/350x230",
-           "https://placehold.it/350x230"
+             "https://images.almundo.com/201/2000000/1350000/1343800/1343731/1343731_71_b.jpg",
+             "https://images.almundo.com/201/1000000/10000/6300/6260/6b25e36d_b.jpg",
+             "https://images.almundo.com/201/1000000/10000/8100/8092/8092_106_b.jpg"
          ],
          "amenities": {
            "wifi": "wifi",
@@ -126,9 +126,9 @@ class HotelResultService
          "stars": "4",
          "price": 5050,
          "images": [
-           "https://images.almundo.com/201/1000000/30000/25000/24954/24954_678_b.jpg",
-           "https://placehold.it/350x230",
-           "https://placehold.it/350x230"
+             "https://images.almundo.com/201/1000000/30000/25000/24954/24954_678_b.jpg",
+             "https://images.almundo.com/201/1000000/10000/6300/6260/6b25e36d_b.jpg",
+             "https://images.almundo.com/201/1000000/10000/8100/8092/8092_106_b.jpg"
          ],
          "amenities": {
              "wifi": "wifi",
@@ -147,8 +147,8 @@ class HotelResultService
            "price": 1203,
            "images": [
                "https://images.almundo.com/201/1000000/30000/25000/24954/24954_678_b.jpg",
-               "https://placehold.it/350x230",
-               "https://placehold.it/350x230"
+               "https://images.almundo.com/201/1000000/10000/6300/6260/6b25e36d_b.jpg",
+               "https://images.almundo.com/201/1000000/10000/8100/8092/8092_106_b.jpg"
            ],
            "amenities": {
                "par": "Parking"
@@ -164,8 +164,8 @@ class HotelResultService
            "price": 7777,
            "images": [
                "https://images.almundo.com/201/1000000/30000/25000/24954/24954_678_b.jpg",
-               "https://placehold.it/350x230",
-               "https://placehold.it/350x230"
+               "https://images.almundo.com/201/1000000/10000/6300/6260/6b25e36d_b.jpg",
+               "https://images.almundo.com/201/1000000/10000/8100/8092/8092_106_b.jpg"
            ],
            "amenities": {
                "wifi": "wifi",
