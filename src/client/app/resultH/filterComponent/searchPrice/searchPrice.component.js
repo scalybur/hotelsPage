a91fb3559153bb305fc31fd9dd59b84f2d9cbb42
nahuelcabrera <nahuelcabrerawork@gmail.com>
2017-05-10 14:33:15 -0300

    filterStarsIssue

--- src/client/app/resultH/filterComponent/searchPrice/searchPrice.component.js
+++ src/client/app/resultH/filterComponent/searchPrice/searchPrice.component.js
@@ -1,42 +1,52 @@
-(function (){
+(function ()
+{
 
     'use strict';
 
     angular
         .module('filterModule')
-        .component('searchPriceComponent',{
+        .component('searchPriceComponent',
+            {
             controller: filterNightController,
-            require: {
+            require:
+                {
                 hotelsResultController : '^hotelsResultComponent'
-            },
-            bindings:{
+                },
+            bindings:
+                {
                 "filters" : '<'
-            },
+                },
             templateUrl: "resultH/filterComponent/searchPrice/searchPrice.html"
         });
 
-    function filterNightController (){
+    function filterNightController ()
+    {
         let self = this;
 
-        this.filterNight = function (){
+        this.filterNight = function ()
+        {
             let hotels = self.hotelsResultController.hotels;
             let filters = self.filters;
 
-            self.hotelsResultController.filterOnHotels = hotels.filter(function (hotel){
-                if(filters.price.priceMin <= hotel.price && filters.price.priceMax >= hotel.price){
+            self.hotelsResultController.filterOnHotels = hotels.filter(function (hotel)
+            {
+                if(filters.price.priceMin <= hotel.price && filters.price.priceMax >= hotel.price)
+                {
                     return hotel;
                 }
             });
         };
 
-        this.slider = {
+        this.slider =
+            {
             value: 150,
-            options: {
+            options:
+                {
                 minRange: 200,
                 noSwitching: true,
                 pushRange: true,
                 onChange : this.filterNight
-            }
+                }
         };
 
     }
